728ab3a7d70f30599e01c96f87616808daf64299
2363702560@qq.com <2363702560@qq.com>
2019-05-06 09:39:57 +0800

    19 commit

--- c++_performance/metaprogram/Test0/main.cpp
+++ c++_performance/metaprogram/Test0/main.cpp
@@ -15,12 +15,27 @@ unsigned binary1(unsigned long N)
 {
 	return (0 == N) ? 0 : (N % 10 + 2 * binary1(N / 10));
 }
+unsigned binary2(unsigned long N)
+{
+	unsigned mode = 0;
+	unsigned base = 1;
+	unsigned num = 0;
+	while (N)
+	{
+		mode = N % 10;
+		N /= 10;
+		num += mode * base;
+		base <<= 1;
+	}
+	return num;
+}
 int main()
 {
 	for (int i = 0; i < LOOP; i++)
 	{
 		//unsigned const three = binary<1101>::value;	// 0.07s
-		unsigned const three = binary1(1101);	// 0.07s
+		//unsigned const three = binary1(1101);			// 0.020s
+		unsigned const three = binary2(1101);			// 0.020s
 	}
 
 	return 0;
