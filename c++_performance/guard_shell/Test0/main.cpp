4b3050c7ef7746a0d1659d2ff8c40d8bd100825e
2363702560@qq.com <2363702560@qq.com>
2019-05-10 14:13:05 +0800

    30 commit

---
+++ c++_performance/guard_shell/Test0/main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <iostream>
+using namespace std;
+void init_daemon() {
+	pid_t pid;
+	if ((pid = fork()) < 0)
+	{
+		exit(EXIT_FAILURE);
+	}
+	if (pid > 0) 		// parent must exit
+	{
+		fprintf(stdout, "I am parent, I will exit...!\n");
+		exit(0);
+	}
+	if (setsid() < 0)	// set a new session and escape from current terminal
+	{
+		fprintf(stderr, "%s", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	umask(0);
+	// close stdin stdout stderr
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+}
+int main() {
+	init_daemon();
+	while (true)
+	{
+		cout << "hello world...!" << endl;
+		sleep(10);
+	}
+
+	return 0;
+}
