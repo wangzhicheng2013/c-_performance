d992f8c6ea1ea63e92ee9ee243da5f708a1b3ba5
2363702560@qq.com <2363702560@qq.com>
2019-06-04 19:44:06 +0800

    speed

--- zeg_robot_simulator/src/tests/unit_test.cpp
+++ zeg_robot_simulator/src/tests/unit_test.cpp
@@ -10,7 +10,7 @@
 *  @author                                                                   *
 *  @email                                                                    *
 *  @version  1.0.0                                                           *
-*  @date     2019-06-03                                                      *
+*  @date     2019-06-04                                                      *
 *  @license                                                                  *
 *                                                                            *
 *----------------------------------------------------------------------------*
@@ -21,6 +21,7 @@
 *----------------------------------------------------------------------------*
 *  2019/06/03 | 1.0.0     |                | Add config parser test case     *
 *----------------------------------------------------------------------------*
+*  2019/06/04 | 1.0.0     |                | Add get next pose test case     *
 *****************************************************************************/
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <iostream>
@@ -89,41 +90,122 @@ TEST_CASE("testing configuration parser") {
 	}
 	CHECK(true == succ);
 }
-TEST_CASE("testing pose compute") {
+TEST_CASE("testing pose compute0") {
+	pose_compute pose_compute_obj;
+	pose_compute_obj.msecs = 1000;
+	pose_compute_obj.cur_pose_ = {10, 10, 0.1};
+	pose_compute_obj.speed_ = {1, 0, 10};
+	robot_pose next_pose;
+	pose_compute_obj.get_next_pose(next_pose);
+	CHECK(next_pose.x > 10);
+	CHECK(next_pose.y > 10);
+	CHECK(next_pose.theta > 0);
+
+	pose_compute_obj.cur_pose_ = {10, 10, 10.1};
+	pose_compute_obj.speed_ = {0, 0, 0};
+	pose_compute_obj.get_next_pose(next_pose);
+	CHECK(10 == next_pose.x);
+	CHECK(10 == next_pose.y);
+	CHECK(true == equal(atan2(sin(10.1), cos(10.1)), next_pose.theta));
+
+	pose_compute_obj.msecs = 1000;
+	pose_compute_obj.cur_pose_ = {10, 0, 0};
+	pose_compute_obj.speed_ = {1, 0, 0};
+	pose_compute_obj.get_next_pose(next_pose);
+	CHECK(11 == next_pose.x);
+	CHECK(0 == next_pose.y);
+	CHECK(true == equal(0.0, next_pose.theta));
+}
+TEST_CASE("testing pose compute1") {
+	pose_compute pose_compute_obj;
+	robot_pose next_pose;
+	pose_compute_obj.msecs = 1000;
+	pose_compute_obj.cur_pose_ = {10, 10, 0};
+	pose_compute_obj.speed_ = {0, 0, 1};
+	pose_compute_obj.get_next_pose(next_pose);
+	CHECK(10 == next_pose.x);
+	CHECK(10 == next_pose.y);
+	CHECK(next_pose.theta > 0);
+
+	pose_compute_obj.msecs = 1000;
+	pose_compute_obj.cur_pose_ = {10, 10, 0};
+	pose_compute_obj.speed_ = {0, 1, 0};
+	pose_compute_obj.get_next_pose(next_pose);
+	CHECK(10 == next_pose.x);
+	CHECK(10 == next_pose.y);
+	CHECK(true == equal(0.0, next_pose.theta));
+}
+TEST_CASE("testing get pose trace") {
 	pose_compute pose_compute_obj;
 	pose_compute_obj.msecs = 100;
-	pose_compute_obj.cur_pose = {10, 10, 0.1};
-	pose_compute_obj.speed = {100, 0, 10};
-	pose_compute_obj();
-	CHECK(pose_compute_obj.next_pose.x > 0);
-	CHECK(pose_compute_obj.next_pose.y > 0);
+	pose_compute_obj.cur_pose_ = {10, 10, 0};
+	pose_compute_obj.destination_pose_ = {11, 10, 0};
+	pose_compute_obj.speed_ = {1, 0, 0};
 
-	pose_compute_obj.cur_pose = {10, 10, 10.1};
-	pose_compute_obj.speed = {0, 0, 0};
-	pose_compute_obj();
-	CHECK(10 == pose_compute_obj.next_pose.x);
-	CHECK(10 == pose_compute_obj.next_pose.y);
+	vector<robot_pose>pose_trace;
+	CHECK(true == pose_compute_obj.get_pose_trace(pose_trace));
+	CHECK(10 == pose_trace.size());
 
-	pose_compute_obj.msecs = 1000;
-	pose_compute_obj.cur_pose = {10, 0, 0};
-	pose_compute_obj.speed = {1, 0, 0};
-	pose_compute_obj();
-	CHECK(11 == pose_compute_obj.next_pose.x);
-	CHECK(0 == pose_compute_obj.next_pose.y);
+	pose_compute_obj.cur_pose_ = {10, 10, 0.88};
+	pose_compute_obj.destination_pose_ = {10, 10, 1.88};
+	pose_compute_obj.speed_ = {0, 0, 1};
 
-	pose_compute_obj.msecs = 1000;
-	pose_compute_obj.cur_pose = {10, 10, 0};
-	pose_compute_obj.speed = {0, 0, 1};
-	pose_compute_obj();
-	cout << pose_compute_obj.next_pose.x << endl;
-	cout << pose_compute_obj.next_pose.y << endl;
-	CHECK(pose_compute_obj.next_pose.x > 0);
-	CHECK(pose_compute_obj.next_pose.y > 0);
+	CHECK(true == pose_compute_obj.get_pose_trace(pose_trace));
+	pose_compute_obj.get_pose_trace(pose_trace);
+	CHECK(0 == pose_trace.size());
 
-	pose_compute_obj.msecs = 1000;
-	pose_compute_obj.cur_pose = {10, 10, 0};
-	pose_compute_obj.speed = {0, 1, 0};
-	pose_compute_obj();
-	CHECK(10 == pose_compute_obj.next_pose.x);
-	CHECK(10 == pose_compute_obj.next_pose.y);
+	double x =  atan2(1.0, 1.0);
+	pose_compute_obj.cur_pose_ = {10, 10, x};
+	pose_compute_obj.destination_pose_ = {11, 11, 0};
+	pose_compute_obj.speed_ = {1, 0, 0.1};
+
+	CHECK(true == pose_compute_obj.get_pose_trace(pose_trace));
+	CHECK(pose_trace.size() > 0);
+
+	x =  atan2(1.0, 0.0);
+	pose_compute_obj.cur_pose_ = {10, 10, x};
+	pose_compute_obj.destination_pose_ = {10, 11, 0};
+	pose_compute_obj.speed_ = {1, 0, 0.1};
+
+	CHECK(true == pose_compute_obj.get_pose_trace(pose_trace));
+	CHECK(pose_trace.size() > 0);
+
+	pose_compute_obj.cur_pose_ = {10, 10, 0};
+	pose_compute_obj.destination_pose_ = {10, 11, 0};
+	pose_compute_obj.speed_ = {1, 0, 0.1};
+
+	CHECK(false == pose_compute_obj.get_pose_trace(pose_trace));
+}
+TEST_CASE("testing adjust pose angle") {
+	pose_compute pose_compute_obj;
+	pose_compute_obj.msecs = 100;
+	pose_compute_obj.cur_pose_ = {10, 10, 0};
+	pose_compute_obj.destination_pose_ = {11, 10, 0};
+	pose_compute_obj.speed_ = {1, 0, 10};
+	CHECK(false == pose_compute_obj.adjust_pose_angle());
+
+	pose_compute_obj.cur_pose_ = {10, 10, 2.1};
+	pose_compute_obj.destination_pose_ = {11, 10, 10.90};
+	pose_compute_obj.speed_ = {1, 0, 0.1};
+	CHECK(true == pose_compute_obj.adjust_pose_angle());
+
+	pose_compute_obj.cur_pose_ = {10, 10, 2.1};
+	pose_compute_obj.destination_pose_ = {11, 10, 10.90};
+	pose_compute_obj.speed_ = {1, 0, 0};
+	CHECK(false == pose_compute_obj.adjust_pose_angle());
+}
+TEST_CASE("testing rotate robot pose") {
+	pose_compute pose_compute_obj;
+	pose_compute_obj.msecs = 100;
+	pose_compute_obj.cur_pose_ = {10, 10, 2.1};
+	pose_compute_obj.destination_pose_ = {11, 10, 10.90};
+	pose_compute_obj.speed_ = {1, 0, 0.1};
+	CHECK(true == pose_compute_obj.adjust_pose_angle());
+
+	vector<robot_pose>pose_trace;
+	pose_compute_obj.rotate_robot_pose(pose_trace);
+	CHECK(pose_trace.size() > 0);
+	CHECK(10 == pose_compute_obj.destination_pose_.y);
+	CHECK(10.90 == pose_compute_obj.destination_pose_.theta);
+	CHECK(1 == pose_compute_obj.speed_.vx);
 }
--- zeg_robot_simulator/src/zeg_robot_define.hpp
+++ zeg_robot_simulator/src/zeg_robot_define.hpp
@@ -9,7 +9,7 @@
 *  @author                                                                   *
 *  @email                                                                    *
 *  @version  1.0.0                                                           *
-*  @date     2019-06-03                                                      *
+*  @date     2019-06-04                                                      *
 *  @license                                                                  *
 *                                                                            *
 *----------------------------------------------------------------------------*
@@ -17,16 +17,19 @@
 *  <Date>     | <Version> | <Author>       | <Description>                   *
 *----------------------------------------------------------------------------*
 *  2019/06/03 | 1.0.0     |                | Create file                     *
-*----------------------------------------------------------------------------*                                                            *
+*----------------------------------------------------------------------------*
+*  2019/06/04 | 1.0.0     |                | Add get_next_pose               *                                                            *
 *****************************************************************************/
 #ifndef SRC_ZEG_ROBOT_DEFINE_HPP_
 #define SRC_ZEG_ROBOT_DEFINE_HPP_
 #include <stdint.h>
 #include <cmath>
+#include <vector>
 #include "msgpack.hpp"
 namespace zeg_robot_simulator {
 using namespace std;
 static const double M_EPS = 0.00000001;
+static const double M_PI_ = 3.1415926535898;
 static const double DEG2RAD_ZEG = 0.017453292519943;
 template <typename T>
 inline bool equal(T x, T y) {
@@ -60,16 +63,34 @@ struct robot_pose {
     double theta;
 	MSGPACK_DEFINE(x, y, theta);
 };
-robot_pose && operator + (const robot_pose &p0, const robot_pose &p1) {
-	return move(robot_pose(p0.x + p1.x, p0.y + p1.y, p0.theta + p1.theta));
+inline void add(const robot_pose &p0, const robot_pose &p1, robot_pose &p) {
+	p.x = p0.x + p1.x;
+	p.y = p0.y + p1.y;
+	p.theta = p0.theta + p1.theta;
 }
-robot_pose && absolute_sum(const robot_pose &p0, const robot_pose &p1) {
+inline double direction(const robot_pose &src_pose, const robot_pose &target_pose) {
+	return atan2(target_pose.y - src_pose.y, target_pose.x - src_pose.x);
+}
+double normalize(double angle) {
+	if (fabs(angle) > M_PI_ * 2) {
+		angle = angle - long(angle / M_PI_ * 2) * M_PI_ * 2;
+	}
+	while (angle > M_PI_) {
+		angle -= M_PI * 2;
+	}
+	while (angle <= -M_PI_) {
+		angle += M_PI_ * 2;
+	}
+	return angle;
+}
+void absolute_sum(const robot_pose &p0, const robot_pose &p1, robot_pose &p) {
 	double s = sin(p0.theta);
 	double c = cos(p0.theta);
-	robot_pose ans(c * p1.x - s * p1.y, s * p1.x + c * p1.y, p1.theta);
-	ans = ans + p0;
-    ans.normalize();
-    return move(ans);
+	p.x = c * p1.x - s * p1.y;
+	p.y = s * p1.x + c * p1.y;
+	p.theta = p1.theta;
+	add(p, p0, p);
+    p.normalize();
 }
 struct robot_speed {
 	robot_speed(double vx = 0, double vy = 0, double w = 0) : vx(vx), vy(vy), w(w) {
@@ -93,30 +114,90 @@ inline double distance(const robot_pose &a, const robot_pose &b) {
 }
 class pose_compute {
 public:
-	void operator () () {
+	pose_compute() {
+		msecs = 100;
+	}
+	pose_compute(int msecs,
+			const robot_pose &cur_pose,
+			const robot_pose &destination_pose,
+			const robot_speed &speed) : msecs(msecs),
+					cur_pose_(cur_pose), destination_pose_(destination_pose), speed_(speed) {
+	}
+	virtual ~pose_compute() = default;
+public:
+	void get_next_pose (robot_pose &next_pose) {
 		 robot_pose motion;
-		 double v = speed.vx;
-		 double w = deg2rad(speed.w);
-		 if (equal(speed.w, 0.0)) {
-			 motion.x = v * msecs / 1000.0;
-		 }
-		 else if (equal(v, 0.0)) {
-			 motion.theta = w * msecs / 1000.0;
-		 }
-		 else {
+		 double v = speed_.vx;
+		 double w = deg2rad(speed_.w);
+		 if (!equal(speed_.w, 0.0) && !equal(v, 0.0)) {
 			 double r = fabs(v / w);
 			 double theta = fabs(w * msecs / 1000.0);
 			 motion.x = sin(theta) * r * sign(v);
 			 motion.y = ((1 - cos(theta)) * r) * sign(v) * sign(w);
 			 motion.theta = theta * sign(w);
 		 }
-		 next_pose = absolute_sum(cur_pose, motion);
+		 else if (equal(speed_.w, 0.0)) {
+			 motion.x = v * msecs / 1000.0;
+		 }
+		 else {
+			 motion.theta = w * msecs / 1000.0;
+		 }
+		 absolute_sum(cur_pose_, motion, next_pose);
+	}
+	bool get_pose_trace(vector<robot_pose>&pose_trace) {
+		pose_trace.clear();
+		double s = speed_.vx * msecs / 1000;
+		double d = distance(cur_pose_, destination_pose_);
+		int loop = ceil(d / (msecs / 1000.0));
+		int count = 0;
+		robot_pose next_pose;
+		while (d > s) {
+			get_next_pose(next_pose);
+			pose_trace.emplace_back(next_pose);
+			cur_pose_ = next_pose;
+			d = distance(cur_pose_, destination_pose_);
+			if (++count > loop) {
+				return false;
+			}
+		}
+		return true;
+	}
+	bool get_pose_trace_with_angle(vector<robot_pose>&pose_trace) {
+		pose_trace.clear();
+		return true;
+	}
+	bool adjust_pose_angle() {
+		if (speed_.w <= 0) {
+			return false;
+		}
+		double normalized_value = normalize(destination_pose_.theta - cur_pose_.theta);
+		if (fabs(normalized_value) < speed_.w * msecs / 1000) {
+			cur_pose_.theta = destination_pose_.theta;
+			return false;
+		}
+		return true;
+	}
+	void rotate_robot_pose(vector<robot_pose>&pose_trace) {
+		pose_trace.clear();
+		robot_pose save_destination_pose = destination_pose_;
+		robot_speed save_speed = speed_;
+		destination_pose_.x = cur_pose_.x;
+		destination_pose_.y = cur_pose_.y;
+		speed_.vx = 0;
+		robot_pose next_pose;
+		do {
+			get_next_pose(next_pose);
+			pose_trace.emplace_back(next_pose);
+			cur_pose_ = next_pose;
+		}while (true == adjust_pose_angle());
+		destination_pose_ = save_destination_pose;
+		speed_ = save_speed;
 	}
 public:
 	int msecs;				// ms
-	robot_pose cur_pose;
-	robot_speed speed;
-	robot_pose next_pose;
+	robot_pose cur_pose_;
+	robot_pose destination_pose_;
+	robot_speed speed_;
 };
 }
 
