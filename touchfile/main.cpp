144d03ad338e04ef146dabff2ed35420e6478e53
2363702560@qq.com <2363702560@qq.com>
2019-06-09 17:59:50 +0800

    utime

--- touchfile/main.cpp
+++ touchfile/main.cpp
@@ -2,10 +2,15 @@
 #include <fstream>
 #include <experimental/filesystem>
 //https://zh.cppreference.com/w/cpp/filesystem/file_time_type
+//https://www.jb51.net/article/71804.htm
 using namespace std;
 using namespace experimental::filesystem;
 int main() {
-
+	path p = current_path();
+	p += "/11.txt";
+	cout << p << endl;
+	ofstream(p.c_str()).put('A');
+	auto ftime = last_write_time(p);
 	
 	return 0;
 }
