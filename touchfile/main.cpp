83bdd6703257b0c4546b3cf0dd05a7e54a849d9b
2363702560@qq.com <2363702560@qq.com>
2019-06-09 17:51:47 +0800

    main

--- touchfile/main.cpp
+++ touchfile/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <experimental/filesystem>
+//https://zh.cppreference.com/w/cpp/filesystem/file_time_type
 using namespace std;
+using namespace experimental::filesystem;
 int main() {
-	qw;
 
 	
 	return 0;
