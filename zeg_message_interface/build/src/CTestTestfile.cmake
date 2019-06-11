5bae623fa7ee270382bac2280d41a06c3efcd429
2363702560@qq.com <2363702560@qq.com>
2019-06-11 20:18:16 +0800

    pose compute

--- zeg_message_interface/build/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/c++/c-_performance/zeg_message_interface/src
-# Build directory: /opt/c++/c-_performance/zeg_message_interface/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- zeg_message_interface/build/src/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/c++/c-_performance/zeg_message_interface/src/tests
-# Build directory: /opt/c++/c-_performance/zeg_message_interface/build/src/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_test "unit_test")
