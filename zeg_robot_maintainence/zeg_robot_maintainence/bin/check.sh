57055497c0b7bcd3f49cd540808bcbbe8f63a420
2363702560@qq.com <2363702560@qq.com>
2019-07-19 10:11:36 +0800

    de

--- zeg_robot_maintainence/zeg_robot_maintainence/bin/check.sh
+++
@@ -1,5 +0,0 @@
-rm -rf *out.*
-valgrind --tool=memcheck --leak-check=full ./unit_test
-#valgrind --tool=callgrind ./unit_test
-valgrind --tool=cachegrind ./unit_test
-#valgrind --tool=helgrind ./unit_test
